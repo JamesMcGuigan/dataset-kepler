b4b3c9adf456ccf005dc7cf0eec69ac186640a58
JamesMcGuigan <james.mcguigan.github@gmail.com>
2020-01-26 05:43:27 +0000

    Notebook | 12_Disposition.ipynb | Correlation Matrices

--- src/utilities.py
+++ src/utilities.py
@@ -1,10 +1,18 @@
 import pandas as pd
 from pydash import py_ as _
 
-def onehot_encode_comments( dataset: pd.DataFrame, column: str, sep='---', inplace=False ) -> pd.DataFrame:
+def onehot_encode_comments( dataset: pd.DataFrame, column: str, sep='---', without=['NO_COMMENT'] ) -> pd.DataFrame:
     comments        = dataset[column].apply(lambda str: str.split('---'))
-    unique_comments = _(comments.tolist()).flatten().uniq().sort_by().filter().value()
-    onehot          = pd.DataFrame(
+    unique_comments = (
+        _(comments.tolist())
+            .flatten()
+            .uniq()
+            .filter()
+            .without( * list(without) )
+            .sort_by()
+            .value()
+    )
+    onehot = pd.DataFrame(
         comments.map(lambda lst: {
             f'{column}_{key}': int(key in lst) for key in unique_comments
         }).tolist(),
